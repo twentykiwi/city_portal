7e053db610f08ca319087d3dc6e3497e152dab4f
Twenty Kiwi <twentykiwi@gmail.com>
2019-04-03 02:02:15 +0530

    new changes in signup

--- app/src/main/java/com/example/city_portal/signup.java
+++ app/src/main/java/com/example/city_portal/signup.java
@@ -15,6 +15,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,7 +28,7 @@ import java.util.Map;
 public class signup extends AppCompatActivity {
     private EditText name,phoneno,username,password,confirmPass;
     private Button btn_register;
-    private static  String URL_REGIST="http://123.0.0.1:80/city_portal/user1.php";
+    AwesomeValidation awesomeValidation;
 
 
     @Override
@@ -33,66 +36,41 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        updateUI();
+
+
+
+    }
+
+    private void updateUI(){
         name=findViewById(R.id.name);
         phoneno=findViewById(R.id.phoneno);
         username=findViewById(R.id.username);
         password=findViewById(R.id.password);
         btn_register=findViewById(R.id.btn_regist);
-        btn_register.setOnClickListener(new View.OnClickListener() {
+
+        // to validate the confirmation of another field
+        String regexPassword = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d])(?=.*[~`!@#\\$%\\^&\\*\\(\\)\\-_\\+=\\{\\}\\[\\]\\|\\;:\"<>,./\\?]).{8,}";
+
+        awesomeValidation.addValidation(signup.this, R.id.name, "[a-zA-Z\\s]+", R.string.name_err);
+        awesomeValidation.addValidation(signup.this, R.id.phoneno, RegexTemplate.TELEPHONE, R.string.mobile_err);
+        awesomeValidation.addValidation(signup.this, R.id.username, "[A-Za-z0-9_]+", R.string.name_err);
+        awesomeValidation.addValidation(signup.this, R.id.password, regexPassword, R.string.password_err);
+        awesomeValidation.addValidation(signup.this, R.id.confirm_password, R.id.password, R.string.confirm_password_err);
+
+        btn_register.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
-                Regist();
-
+                if(awesomeValidation.validate()){
+                    Toast.makeText(signup.this,"Data Received Successfully",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(signup.this,"Error",Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
-
-    }
-    private  void Regist(){
-        btn_register.setVisibility(View.GONE);
-
-        final String name = this.name.getText().toString().trim();
-        final String phoneno = this.name.getText().toString().trim();
-        final String username = this.name.getText().toString().trim();
-        final String password= this.name.getText().toString().trim();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try{
-                    JSONObject jsonObject= new JSONObject(response);
-                    String success = jsonObject.getString("success");
-                    if(success.equals("1")){
-                        Toast.makeText(signup.this, "Register success!!", Toast.LENGTH_LONG).show();
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Toast.makeText(signup.this, "Register error!!"+e.toString(), Toast.LENGTH_LONG).show();
-                    btn_register.setVisibility(View.VISIBLE);
-                }
-
-            }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(signup.this, "Register error!!"+error.toString(), Toast.LENGTH_LONG).show();
-                        btn_register.setVisibility(View.VISIBLE);
-
-                    }
-                })
-        {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> params= new HashMap<>();
-                params.put("name",name);
-                params.put("phone",phoneno);
-                params.put("username",username);
-                params.put("password",password);
-                return params;
-            }
-        };
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
     }
 }
