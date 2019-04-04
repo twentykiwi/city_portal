532b50cec9127946a4a2f38c64bd521d217f8c38
Twenty Kiwi <twentykiwi@gmail.com>
2019-04-04 17:49:51 +0530

    signup code updated

---
+++ app/src/main/java/com/example/city_portal/TestActivity.java
@@ -0,0 +1,27 @@
+package com.example.city_portal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class TestActivity extends AppCompatActivity {
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        this.listView = (ListView) findViewById(R.id.listView);
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<String> quotes = databaseAccess.getQuotes();
+        databaseAccess.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(TestActivity.this, android.R.layout.simple_list_item_1, quotes);
+        this.listView.setAdapter(adapter);
+    }
+}
